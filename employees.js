f77f1facf716fe478476874c6a22d5136b6e29e9
Richard Wang <richardchacha@hotmail.com>
2020-02-16 15:39:46 -0800

    add initial console.log output

--- employees.js
+++ employees.js
@@ -1,4 +1,10 @@
 'use strict'
+console.clear();
 
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+const prompts = require('./prompts');
+
+console.log(prompts.brand);
+
